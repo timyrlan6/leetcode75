751247b97efd2c3de2d2118cc619143014c414bf
DESKTOP-UC2OCF1\Admin <timyrlan6@gmail.com>
2026-05-04 17:31:28 +0300

    2300_SuccessfulPairsOfSpellsAndPotions

--- LeetCode75/LeetCodeTests/BinarySearch/Medium/_2300_SuccessfulPairsOfSpellsAndPotions/2300_SuccessfulPairsOfSpellsAndPotions.cs
+++ LeetCode75/LeetCodeTests/BinarySearch/Medium/_2300_SuccessfulPairsOfSpellsAndPotions/2300_SuccessfulPairsOfSpellsAndPotions.cs
@@ -20,8 +20,33 @@ namespace LeetCodeTests.BinarySearch.Medium._2300_SuccessfulPairsOfSpellsAndPoti
     public class Solution
     {
         public int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+        {        
+            potions = potions.Order().ToArray();
+            spells = spells.Select(c=>(int)Math.Ceiling((double)success/c)).ToArray();
+
+
+            for (int i = 0; i < spells.Length; i++)
+            {
+                var search = Search(potions, 0, potions.Length - 1, spells[i]);
+                spells[i] = potions.Length - search;
+            }
+
+            return spells;
+        }
+
+        private int Search(int[] potions, int left, int right, long search)
         {
-            return default;
+            while (left <= right)
+            {
+                var mid = left + (right - left) / 2;
+
+                if (potions[mid] >= search)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+
+            return left;
         }
     }
 
